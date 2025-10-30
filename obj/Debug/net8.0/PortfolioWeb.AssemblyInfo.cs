4fcae842f4fc02f34e6baa6e4fa605309f70fe18
TheTeleporter9 <the.code.load1@gmail.com>
2025-10-30 18:29:34 +0200

    Clean Git index and ignore node_modules

--- obj/Debug/net8.0/PortfolioWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9007d75af811eba079c649016c1ea11a448c5762")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91518dbefcb9658756d183e19614f9cc95d6fccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/PortfolioWeb.AssemblyInfo.cs
+++ obj/Release/net8.0/PortfolioWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8903b7c6f68881e515fb90190dc94dcbecad274a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91518dbefcb9658756d183e19614f9cc95d6fccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
